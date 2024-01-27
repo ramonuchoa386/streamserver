76b2061ca198b8251f8642c8ec516efba2e6564f
vt415204 <ramon.pinho@vtal.com>
2024-01-27 20:27:08 -0300

    feat: update exposed ports

--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-# docker run -p 80:80 -e RTSP_URL=rtsp://localhost:8554/live/stream streamserver
-
-docker run -p 8090:8090 -p 1935:1935 --name streamserver streamserver
+docker run -p 8090:8090 -p 1935:1935 -p 554:554 -p 22:22 --name streamserver streamserver
